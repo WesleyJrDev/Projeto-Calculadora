3a5e0a1fddd195623a8f4a2b1bb22fc1394fe9ab
Wesley Jr <wesley_damazio@hotmail.com>
2023-06-28 20:11:41 -0300

    .

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+  const previusOperationText = document.querySelector("#previous-operations");
+  const currrentOperationText = document.querySelector("#current-operation");
+  const buttons = document.querySelectorAll("#buttons-container button");
+
+  class Calculator{
+    
+  }
