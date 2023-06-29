e2de2f503236fd0a888612c6ab0146f946176ca8
Wesley Jr <wesley_damazio@hotmail.com>
2023-06-29 19:08:17 -0300

    calculadora

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,22 @@
+//   Seleção dos Elementos 
+
   const previusOperationText = document.querySelector("#previous-operations");
+
   const currrentOperationText = document.querySelector("#current-operation");
+
   const buttons = document.querySelectorAll("#buttons-container button");
 
+// Lógica de aplicação da Calculadora
+
   class Calculator{
-    
+
   }
+
+// Eventos Utilizados
+
+   buttons.forEach((btn)=> {
+    btn.addEventListener("click",(e) => {
+        const value = e.target.innerText;
+        console.log(value);
+    });
+   });
