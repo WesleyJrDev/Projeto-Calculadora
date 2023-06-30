e18d6d810ae7becc8c733e0697461f0fa033fdf3
Wesley Jr <wesley_damazio@hotmail.com>
2023-06-29 21:22:56 -0300

    .

--- JS/script.js
+++ JS/script.js
@@ -17,6 +17,11 @@
    buttons.forEach((btn)=> {
     btn.addEventListener("click",(e) => {
         const value = e.target.innerText;
-        console.log(value);
+        
+        if (+value >= 0 || value ==="."){
+          console.log(value);
+        } else {
+          console.log("Op:" + value);
+        }
     });
    });
